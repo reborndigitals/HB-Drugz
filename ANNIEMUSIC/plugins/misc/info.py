f4bfb6b05dce0b4965a92dd29995111806b3037a
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-08-08 08:25:04 +0530

    info.py

--- ANNIEMUSIC/plugins/misc/info.py
+++ ANNIEMUSIC/plugins/misc/info.py
@@ -13,10 +13,10 @@ import random
 
 # List of Annie's photos
 anniephoto = [
-    "https://graph.org/file/ffdb1be822436121cf5fd.png",
-    "https://graph.org/file/ffdb1be822436121cf5fd.png",
-    "https://graph.org/file/ffdb1be822436121cf5fd.png",
-    "https://graph.org/file/ffdb1be822436121cf5fd.png"
+    "https://telegra.ph/file/07fd9e0e34bc84356f30d.jpg",
+    "https://telegra.ph/file/3c4de59511e179018f902.jpg",
+    "https://telegra.ph/file/07fd9e0e34bc84356f30d.jpg",
+    "https://telegra.ph/file/3c4de59511e179018f902.jpg"
 ]
 
 # Function to get font
